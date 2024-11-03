72f2adb192ffdfccf1c9edacd5ff08e96ae39593
Tasim <tasimbillah611@gmail.com>
2024-11-03 10:19:00 +0600

    Different way to handle events in React

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+  // default function 
+  function handleClick() {
+    alert("Button One Clicked")
+  }
+  // arrow function
+  const  handleClick2 = () => {
+    alert("Button Two Clicked")
+  }
+
+  const add  = (a, b) => {
+    alert("Summation of 10 + 2 is " + (a+b));
+  }
+
+  return (
+    <>
+     <h2>React Core Concepts</h2>
+     <button className='margin' onClick={handleClick}>click button 1 </button>
+     <button className='margin' onClick={handleClick2}>click button 2 </button>
+     <button className='margin' onClick={() => {alert("Button Three Clicked")} }>click button 3 </button>     
+     <button className='margin' onClick={() => {alert(`The sum of : ${10} and ${2} is : ${10 + 2}`)}}>The Summation of 10 and 2 </button>     
+     {/* More Easier Way */}
+     <button className='margin' onClick={() => {add(10, 2)}}>The Summation of 10 and 2  </button>
+
+
+    </>
+  )
+}
+
+export default App
